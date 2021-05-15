990a27c33861f328513ff55aabe1c97769d35d93
oraum <oraum@users.noreply.github.com>
2021-05-15 14:18:51 +0200

    VTMSite-7 Make it possible to load and save characters
    * tests with test harness

--- src/app/import-export/import-export.component.spec.ts
+++ src/app/import-export/import-export.component.spec.ts
@@ -4,10 +4,14 @@ import {ImportExportComponent} from './import-export.component';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatButtonHarness} from '@angular/material/button/testing';
 
 describe('ImportExportComponent', () => {
   let component: ImportExportComponent;
   let fixture: ComponentFixture<ImportExportComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,9 +26,16 @@ describe('ImportExportComponent', () => {
     fixture = TestBed.createComponent(ImportExportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable import button on start', async () => {
+    const importBut = await loader.getHarness(MatButtonHarness.with({text: 'Import'}));
+
+    expect(await importBut.isDisabled()).toBeTrue();
   });
 });
--- src/app/save-button/save-button.component.spec.ts
+++ src/app/save-button/save-button.component.spec.ts
@@ -4,15 +4,22 @@ import {SaveButtonComponent} from './save-button.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconTestingModule} from '@angular/material/icon/testing';
 import {MatIcon} from '@angular/material/icon';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import {MatDialogHarness} from '@angular/material/dialog/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 describe('SaveButtonComponent', () => {
   let component: SaveButtonComponent;
   let fixture: ComponentFixture<SaveButtonComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SaveButtonComponent, MatIcon],
-      imports: [MatDialogModule, MatIconTestingModule]
+      imports: [MatDialogModule, MatIconTestingModule, NoopAnimationsModule, MatTooltipModule]
     })
       .compileComponents();
   });
@@ -21,9 +28,24 @@ describe('SaveButtonComponent', () => {
     fixture = TestBed.createComponent(SaveButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy();
+    const button = await loader.getHarness(MatButtonHarness);
+    expect(button).toBeTruthy();
+
+  });
+
+  it('should open dialog', async () => {
+    component.character = {freebiePoints: 10, name: 'Test'};
+    const button = await loader.getHarness(MatButtonHarness);
+    await button.click();
+    const dialog = await TestbedHarnessEnvironment.documentRootLoader(fixture).getHarness(MatDialogHarness);
+    expect(dialog).toBeTruthy();
+    await dialog.close();
+
+    expect(component.character.name).toEqual('Test');
   });
 });
